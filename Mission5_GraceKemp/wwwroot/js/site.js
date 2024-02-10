81b9df3b714468543d3a30570f4a802d62cb3670
gogden245 <go245@byu.edu>
2024-02-09 23:44:21 -0700

    Add project files.

---
+++ Mission5_GraceKemp/wwwroot/js/site.js
@@ -0,0 +1,14 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$("#calculate-btn").click(function () {
+    // Declare what the variable 'total' is
+    var total = $("#hours").val() * 50;
+    // Alter the user of the total
+    alert("Hours Needed: " + $("#hours").val() +
+        "\nHourly Rate: " + $("#hourly-rate").val() +
+        "\nTotal Cost: " + total.toFixed(2) );
+
+});
